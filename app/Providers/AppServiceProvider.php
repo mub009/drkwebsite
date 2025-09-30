55794d85178ba8d43da767270fa114fea78e728b
NABHAN-NISHAM-KM <nabhannisham5@gmail.com>
2025-09-30 18:06:47 +0530

    upgrade to v2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
+
     }
 }
